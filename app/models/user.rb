a9297a10bbeb00709f5420022e907f922efc93b7
Drew Land <dland@thinkthroughmath.com>
2014-09-11 15:19:43 -0400

    Remove User prefix on authentication methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -36,11 +36,11 @@ class User < ActiveRecord::Base
 
   # Authentication #############################################################
 
-  def User.new_remember_token
+  def new_remember_token
     SecureRandom.urlsafe_base64
   end
 
-  def User.encrypt(token)
+  def encrypt(token)
     Digest::SHA1.hexdigest(token.to_s)
   end
 
