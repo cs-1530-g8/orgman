026997731e84806f8607a71021658633be20aea2
Drew Land <dland@thinkthroughmath.com>
2014-09-12 15:38:36 -0400

    Update user model validations and formatting

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,30 @@
 class User < ActiveRecord::Base
+  # Constants ##################################################################
+
+  # Validations ################################################################
+
+  validates :first_name, presence: true, length: { maximum: 50 }
+  validates :last_name,  presence: true, length: { maximum: 50 }
+  validates :two_p_number,               length: { is: 15 }, allow_blank: true
+  validates :peoplesoft_number,          length: { is: 7 },  allow_blank: true
+
+  has_attached_file :avatar,
+                    styles: { medium: '300x300>', thumb: '100x100>' },
+                    default_url: '/images/:style/missing.png'
+
+  # Scopes #####################################################################
+
+  # Associations ###############################################################
+
+  # Helpers ####################################################################
+
+  def name
+    "#{first} #{last}"
+  end
+
+  # Authentication #############################################################
+
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable,
          :confirmable, :lockable, :timeoutable
 end
