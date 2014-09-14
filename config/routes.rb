21ccab70879681c1a867eee7f6885fafccc94c56
Drew Land <dland@thinkthroughmath.com>
2014-09-13 21:19:11 -0400

    Prettify devise routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,31 @@
 Rails.application.routes.draw do
-  get 'external/index'
+  get 'index' => 'external#index', as: :external_index
 
-  devise_for :users
+  # Add devise routes for users
+  # devise_for :users
+  devise_for :users, skip: [:sessions, :registrations, :password, :confirmation, :unlock]
+
+  # Rename devise routes to look nicer
+  devise_scope :user do
+    get    'login'    => 'devise/sessions#new',         as: :new_user_session
+    post   'login'    => 'devise/sessions#create',      as: :user_session
+    delete 'signout'  => 'devise/sessions#destroy',     as: :destroy_user_session
+
+    get    'signup'   => 'devise/registrations#new',    as: :new_user_registration
+    post   'signup'   => 'devise/registrations#create', as: :user_registration
+    put    'signup'   => 'devise/registrations#update', as: :update_user_registration
+    get    'account'  => 'devise/registrations#edit',   as: :edit_user_registration
+
+    post   'password' => 'devise/passwords#create',      as: :user_password
+    get    'password' => 'devise/passwords#new',         as: :new_user_password
+    get    'password' => 'devise/passwords#edit',        as: :edit_user_password
+
+    post   'confirm'  => 'devise/confirmations#create', as: :user_confirmation
+    get    'confirm'  => 'devise/confirmations#new',    as: :new_user_confirmation
+
+    post   'unlock'   => 'devise/unlocks#create',        as: :user_unlock
+    get    'unlock'   => 'devise/unlocks#new',           as: :new_user_unlock
+  end
 
   root 'external#index'
 end
