d8c91cc590b1c176c8fd91787d069ffb58e8cfb2
Alex Vallejo <vallejo.alex@gmail.com>
2014-11-08 22:01:05 -0500

    Add user id to link

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141108221100) do
+ActiveRecord::Schema.define(version: 20141109025647) do
 
   create_table "links", force: true do |t|
     t.string   "name"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20141108221100) do
     t.datetime "expiration"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
 
   create_table "users", force: true do |t|
