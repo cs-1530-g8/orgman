740dbc0bb9d841959180fdb5284781d726e6d113
Alex Vallejo <vallejo.alex@gmail.com>
2014-11-16 21:45:29 -0500

    Order link display by name

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,3 +1,4 @@
 class Link < ActiveRecord::Base
-  scope :active, -> { where("expiration >= :today", today: Date.today) }
+  scope :active, -> { where("expiration >= :today", today: Date.today)
+                      .order(:name) }
 end
