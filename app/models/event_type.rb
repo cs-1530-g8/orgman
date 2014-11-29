4fe4a7c1cb2ced8c7d34b7a439778c5897590c7b
Alex Vallejo <vallejo.alex@gmail.com>
2014-11-28 20:45:50 -0500

    Enforce mutual exclusivity for event types

--- app/models/event_type.rb
+++ app/models/event_type.rb
@@ -3,11 +3,20 @@ class EventType < ActiveRecord::Base
   before_save { self.name = name.titleize }
 
   has_many :events
+
   validates :name, presence: true, length: { maximum: 50 }
+
   validates :points_required, length: {maximum: 2},
-            numericality: { only_integer: true }, allow_nil: true
+            numericality: { only_integer: true }
+
   validates :percentage_attendance_required, length: {maximum: 3},
-            numericality: { only_integer: true }, allow_nil: true
+            numericality: { only_integer: true }
+
+  validates :points_required, absence: true,
+          if: Proc.new { |e| e.percentage_attendance_required.present? }
+
+  validates :percentage_attendance_required, absence: true,
+          if: Proc.new { |e| e.points_required.present? }
 
   def required?
     self.points_required > 0 || self.percentage_attendance_required > 0
