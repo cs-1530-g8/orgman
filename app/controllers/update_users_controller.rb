0c815d9fbe466ead9e474b819cf0bb8367487277
Alex Vallejo <vallejo.alex@gmail.com>
2014-12-19 16:19:40 -0500

    Add update_status controller method

--- app/controllers/update_users_controller.rb
+++ app/controllers/update_users_controller.rb
@@ -20,4 +20,64 @@ class UpdateUsersController < ApplicationController
     flash[:notice] = 'Status updated successfully'
     redirect_to update_users_path
   end
+
+  def update_positions
+    unless params[:president].blank?
+      user = User.find(params[:president])
+      user.update(position: User.president)
+    end
+
+    unless params[:vice_president].blank?
+      user = User.find(params[:vice_president])
+      user.update(position: User.vice_president)
+    end
+
+    unless params[:secretary].blank?
+      user = User.find(params[:secretary])
+      user.update(position: User.secretary)
+    end
+
+    unless params[:treasurer].blank?
+      user = User.find(params[:treasurer])
+      user.update(position: User.treasurer)
+    end
+
+    unless params[:alumni_relations].blank?
+      user = User.find(params[:alumni_relations])
+      user.update(position: User.alumni_relations)
+    end
+
+    unless params[:risk_manager].blank?
+      user = User.find(params[:risk_manager])
+      user.update(position: User.risk_manager)
+    end
+
+    unless params[:recruitment].blank?
+      user = User.find(params[:recruitment])
+      user.update(position: User.recruitment)
+    end
+
+    unless params[:social].blank?
+      user = User.find(params[:social])
+      user.update(position: User.social)
+    end
+
+    unless params[:amc].blank?
+      user = User.find(params[:amc])
+      user.update(position: User.amc)
+    end
+
+    unless params[:jamc].blank?
+      user = User.find(params[:jamc])
+      user.update(position: User.jamc)
+    end
+
+    unless params[:house_manager].blank?
+      user = User.find(params[:house_manager])
+      user.update(position: User.house_manager)
+    end
+
+    flash[:notice] = 'Positions updated successfully'
+    redirect_to update_users_path
+  end
 end
