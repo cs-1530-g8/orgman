d2ab5911a1c706698c74913ce771d6c719e4c2d8
Alex Vallejo <vallejo.alex@gmail.com>
2014-12-23 18:34:24 -0500

    Add image upload for user profile pictures

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -5,12 +5,14 @@ class RegistrationsController < Devise::RegistrationsController
   def sign_up_params
     params.require(:user).permit(:first_name, :last_name, :email, :password,
                                  :password_confirmation, :peoplesoft_number,
-                                 :two_p_number, :address, :phone_number, :about)
+                                 :two_p_number, :address, :phone_number, :about,
+                                 :avatar)
   end
 
   def account_update_params
     params.require(:user).permit(:first_name, :last_name, :email, :password,
                                  :password_confirmation, :peoplesoft_number,
-                                 :two_p_number, :address, :phone_number, :about)
+                                 :two_p_number, :address, :phone_number, :about,
+                                 :avatar)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141129232552) do
+ActiveRecord::Schema.define(version: 20141223020058) do
 
   create_table "attendances", force: true do |t|
     t.integer  "user_id"
@@ -81,6 +81,10 @@ ActiveRecord::Schema.define(version: 20141129232552) do
     t.boolean  "approved",               default: false, null: false
     t.string   "position"
     t.string   "status"
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
+    t.datetime "avatar_updated_at"
   end
 
   add_index "users", ["approved"], name: "index_users_on_approved"
