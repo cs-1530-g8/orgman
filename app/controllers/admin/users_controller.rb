7c303e3de2f4fca0b8fc0227e145581f446635cd
Alex Vallejo <vallejo.alex@gmail.com>
2014-12-24 01:34:47 -0500

    Add more to user rejection from pending approvals

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -7,21 +7,23 @@ class Admin::UsersController < ApplicationController
   end
 
   def approve_user
-    id = params[:id]
     status = params[:status]
-    user = User.find(id)
+    user = User.find(params[:id])
     if params[:approve] == 'true'
+      ApprovalMailer.accepted(@user).deliver
       user.update(approved: true, status: status)
     end
     redirect_to(pending_approvals_path)
   end
 
   def reject_user
+    rejection_message = params[:rejection_message][:message]
     user = User.find(params[:id])
     if user.destroy
-      flash[:notice] = "#{user.name}'s application was rejected."
+      ApprovalMailer.rejected(user, rejection_message).deliver
+      flash[:notice] = "#{user.name}'s request was rejected."
     else
-      flash[:alert] = "#{user.name}'s application was not rejected. Try again."
+      flash[:alert] = "#{user.name}'s request was not rejected. Try again."
     end
     redirect_to(pending_approvals_path)
   end
