03a376082a8766ac889269331bb17cd968b8adb8
Alex Vallejo <vallejo.alex@gmail.com>
2014-12-27 11:19:07 -0500

    Add excuses controller and decorator

---
+++ app/controllers/attendance/excuses_controller.rb
@@ -0,0 +1,24 @@
+class Attendance::ExcusesController < ApplicationController
+  before_action :authenticate_user!
+
+  def index
+  end
+
+
+  def create
+
+  end
+
+  def destroy
+
+  end
+
+  def pending_excuses
+
+  end
+
+  def process_excuse
+
+  end
+
+end
