947757f84a16fd5852a3d6fa58549d556ad78740
Alex Vallejo <vallejo.alex@gmail.com>
2014-12-27 16:14:56 -0500

    Add routes for excuses

--- app/controllers/attendance/excuses_controller.rb
+++ app/controllers/attendance/excuses_controller.rb
@@ -2,23 +2,18 @@ class Attendance::ExcusesController < ApplicationController
   before_action :authenticate_user!
 
   def index
+    @excuses = current_user.excuses
   end
 
-
   def create
-
   end
 
   def destroy
-
   end
 
   def pending_excuses
-
   end
 
   def process_excuse
-
   end
-
 end
