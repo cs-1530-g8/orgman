4426596e4ba042cdd01d86ef32c9f7e1987d466e
Alex Vallejo <vallejo.alex@gmail.com>
2014-12-27 21:08:23 -0500

    Add excuse create controller action

--- app/controllers/attendance/excuses_controller.rb
+++ app/controllers/attendance/excuses_controller.rb
@@ -7,6 +7,19 @@ class Attendance::ExcusesController < ApplicationController
   end
 
   def create
+    excuse = Excuse.new(excuse_params)
+    excuse.user_id = current_user.id
+    attendance = Attendance.find_by(event_id: excuse.event_id,
+                                    user_id: excuse.user_id)
+    if attendance
+      excuse.attendance_id = attendance.id
+      flash[:notice] = "You have successfully submited an excuse for #{excuse.event.name} on #{format_date(excuse.event.date)}"
+    else
+      flash[:notice] = 'An error occured. Please contact your administrator to submit your excuse manually'
+    end
+
+    excuse.save
+    redirect_to excuses_path
   end
 
   def destroy
@@ -16,5 +29,11 @@ class Attendance::ExcusesController < ApplicationController
   end
 
   def process_excuse
+  end
+
+  private
+
+  def excuse_params
+    params.require(:excuse).permit(:event_id, :reason)
   end
 end
