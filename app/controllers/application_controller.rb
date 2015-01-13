fcd80d36ff6e95f8fb0a895be00f3717809835a2
Alex Vallejo <vallejo.alex@gmail.com>
2015-01-12 23:44:10 -0500

    Add method to authenticate via position

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,11 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   include ApplicationHelper
+
+  def user_has_position(position)
+    unless current_user && current_user.position == position
+      flash[:notice] = "Sorry but you must be the #{position} to view that page"
+      redirect_to root_path
+    end
+  end
 end
