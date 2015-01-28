a14a1ffcf19fc75d415edfdf5cec0197fce21112
Alex Vallejo <vallejo.alex@gmail.com>
2015-01-28 14:03:10 -0500

    Secure update users and pending approvals pages

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,6 +1,7 @@
 class Admin::UsersController < ApplicationController
 
   before_filter :authenticate_user!
+  before_action -> { user_has_position(User.secretary) }
 
   def pending_approvals
     @users = User.where(approved: false)
