198f2a0a344d433dd970a6c66a30947a7f648934
Alex Vallejo <vallejo.alex@gmail.com>
2015-01-29 18:31:35 -0500

    Add positions to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,16 +6,20 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-admin = User.create! first_name: 'Alex', last_name: 'Vallejo',
+admin = User.create!(first_name: 'Alex', last_name: 'Vallejo',
                      email: 'email@example.com', password: 'password',
                      password_confirmation: 'password', approved: true,
-                     status: 'active', position: 'secretary'
+                     status: 'active')
 admin.confirm!
 
-standard_user = User.create! first_name: 'Drew', last_name: 'Land',
+standard_user = User.create!(first_name: 'Drew', last_name: 'Land',
                      email: 'drew@example.com', password: 'password',
                      password_confirmation: 'password', approved: true,
-                     status: 'active'
+                     status: 'active')
 standard_user.confirm!
 
 EventType.create(name: 'Miscellaneous')
+
+Position.create(name: 'President', user_id: 1)
+Position.create(name: 'Treasurer', user_id: 1)
+Position.create(name: 'Secretary', user_id: 1)
