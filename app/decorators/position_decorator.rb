294e81b23c75853e2a8e3ec0444fc9bcd52cc211
Alex Vallejo <vallejo.alex@gmail.com>
2015-02-04 09:39:26 -0500

    Prevent default exec positions from being editable in front end.

--- app/decorators/position_decorator.rb
+++ app/decorators/position_decorator.rb
@@ -3,7 +3,11 @@ class PositionDecorator < Draper::Decorator
 
   def format_name_field
     if object.event_type.nil?
-      h.text_field(:position, :name, value: object.name)
+      if object.is_default_position?
+        h.text_field(:position, :name, value: object.name, disabled: true)
+      else
+        h.text_field(:position, :name, value: object.name)
+      end
     else
       h.text_field(:position, :name, { disabled: true,
                                        value: object.event_type.name })
@@ -13,7 +17,7 @@ class PositionDecorator < Draper::Decorator
   def render_delete_button
     if event_type_id.nil? && id.present? && id > 5
       h.button_to('remove', h.position_path(self), method: :delete,
-                                                    class: 'btn btn-danger')
+                                                   class: 'btn btn-danger')
     end
   end
 end
