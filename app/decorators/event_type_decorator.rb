7485953ac5f228d8cb7d112a1fc206b6cbf7e816
Alex Vallejo <vallejo.alex@gmail.com>
2015-02-07 15:08:26 -0500

    Fix hound issues

--- app/decorators/event_type_decorator.rb
+++ app/decorators/event_type_decorator.rb
@@ -3,16 +3,16 @@ class EventTypeDecorator < Draper::Decorator
 
   def name_field
     if object.name == 'Miscellaneous'
-      h.text_field :event_type, :name, { value: object.name, disabled: true }
+      h.text_field :event_type, :name, value: object.name, disabled: true
     else
-      h.text_field :event_type, :name, { value: object.name }
+      h.text_field :event_type, :name, value: object.name
     end
   end
 
   def delete_link
     if object.name? && object.name != 'Miscellaneous'
       h.link_to 'Delete', h.event_type_path(self), method: :delete,
-        class: 'btn btn-danger btn-sm'
+                                                   class: 'btn btn-danger btn-sm'
     end
   end
 end
