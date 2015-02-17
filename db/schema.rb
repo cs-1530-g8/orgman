ea5cddf382cee095185f64f6260b44f3c3c21cac
Alex Vallejo <vallejo.alex@gmail.com>
2015-02-17 16:30:38 -0500

    Ad extra info to user for org chart

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150212210740) do
+ActiveRecord::Schema.define(version: 20150215194718) do
 
   create_table "attendances", force: true do |t|
     t.integer  "user_id"
@@ -100,6 +100,9 @@ ActiveRecord::Schema.define(version: 20150212210740) do
     t.string   "avatar_content_type"
     t.integer  "avatar_file_size"
     t.datetime "avatar_updated_at"
+    t.string   "division"
+    t.string   "extra_info"
+    t.integer  "parent_id"
   end
 
   add_index "users", ["approved"], name: "index_users_on_approved"
