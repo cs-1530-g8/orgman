c656451fa57f6094fda608394832e4e604d20020
Drew Land <dland@thinkthroughmath.com>
2015-02-17 21:12:28 -0500

    Add link factory

--- spec/factories/factories.rb
+++ spec/factories/factories.rb
@@ -1,14 +1,10 @@
 FactoryGirl.define do
-  sequence(:email)      { |n| "email-#{n}@example.com" }
-  sequence(:first_name) { |n| "FirstName#{n}" }
-  sequence(:last_name)  { |n| "LastName#{n}" }
-
   factory :user do
-    first_name
-    last_name
-    email
+    sequence(:first_name) { |n| "FirstName#{n}" }
+    sequence(:last_name)  { |n| "LastName#{n}" }
+    sequence(:email)      { |n| "email-#{n}@example.com" }
     confirmed_at { Time.now }
-    password "password"
+    password 'password'
     approved true
 
     trait :unconfirmed do
@@ -17,6 +13,17 @@ FactoryGirl.define do
 
     trait :unapproved do
       approved false
+    end
+  end
+
+  factory :link do
+    sequence(:name)      { |n| "link#{n}" }
+    url 'example.com'
+    expiration { DateTime.tomorrow }
+    user
+
+    trait :expired do
+      expiration { DateTime.yesterday }
     end
   end
 end
