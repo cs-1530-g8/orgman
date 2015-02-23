3a8f6a44727beba92eb42e6c360c21ccaacbb375
Alex Vallejo <vallejo.alex@gmail.com>
2015-02-23 13:34:51 -0500

    Add admin interface for org chart

--- app/controllers/org_chart_controller.rb
+++ app/controllers/org_chart_controller.rb
@@ -14,30 +14,47 @@ class OrgChartController < ApplicationController
       options = { allowHtml: true }
 
       chart = GoogleVisualr::Interactive::OrgChart.new(data_table, options)
-      brk
       @charts.push(chart)
     end
   end
 
-  def show
-  end
-
-  def edit
-  end
-
-  def new
+  def admin
+    @divisions = User.uniq.pluck(:division).reject(&:nil?)
   end
 
   def create
+    user = User.find_by(id: params[:user][:existing_id])
+    params[:user][:division] = params[:user][:division].present? ?
+                                               params[:user][:division] :
+                                               params[:user][:existing_division]
+    if user
+      User.update(user_params)
+    else
+      user = User.new(user_params_with_name)
+      user.email = ('a'..'z').to_a.shuffle[0,15].join
+      user.save(validate: false)
+    end
+    redirect_to org_chart_admin_path
   end
 
-  def destroy
+  def remove
+    user = User.find(params[:user][:id])
+    if !user.approved
+      user.destroy
+    else
+      user.save(division: nil, parent_id: nil, extra_info: nil, validate: false)
+    end
+    redirect_to org_chart_admin_path
   end
 
   private
 
   def user_params
-    params.require(:user).permit(:id, :division, :extra_info, :parent_id)
+    params.require(:user).permit(:division, :extra_info, :parent_id)
+  end
+
+  def user_params_with_name
+    params.require(:user).permit(:first_name, :division, :extra_info, :parent_id)
   end
 
   def get_array_for_division(division)
