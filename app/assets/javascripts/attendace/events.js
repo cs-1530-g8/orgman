1487aa9999301789e24e4ed357bdd316880ba25c
Alex Vallejo <vallejo.alex@gmail.com>
2015-03-05 13:39:36 -0500

    Add JS to support roll call

---
+++ app/assets/javascripts/attendace/events.js
@@ -0,0 +1,37 @@
+$(function() {
+  var index = 0;
+  var options = $("#event_attendances_user_ids option");
+  var select_tag = $("#event_attendances_user_ids");
+  var selections = [];
+
+  $("#display_roll_call").click(function() {
+    $("#roll_call").toggle();
+  });
+
+  $("#name_display").text(options[index].text);
+
+  $("#absent").click(function () {
+    nextName();
+  });
+
+  $("#present").click(function () {
+    if (index < options.length) {
+      selections.push(options[index].value);
+      options.filter('[value="' + options[index].value + '"]')
+             .prop('selected', true);
+    }
+    nextName();
+  });
+
+  function nextName() {
+    if (index >= options.length) {
+      alert("You have reached the end of roll call!");
+    } else {
+      index++;
+      select_tag.val(selections).trigger("change");
+      if (index < options.length) {
+        $("#name_display").text(options[index].text);
+      }
+    }
+  }
+});
