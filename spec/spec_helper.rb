e66453a5cf1ee35bef8dabc37419024f214695e6
Alex Vallejo <vallejo.alex@gmail.com>
2015-03-18 19:00:14 -0400

    Add code climate and coveralls

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,13 @@
+require 'coveralls'
+Coveralls.wear!
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
+
+# Code Climate Test Coverage
+# require "codeclimate-test-reporter"
+# CodeClimate::TestReporter.start
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
