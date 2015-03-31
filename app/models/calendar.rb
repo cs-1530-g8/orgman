d0eb2a9270f82e3889c11aa68fb4d0f3ccd4c59c
Alex Vallejo <vallejo.alex@gmail.com>
2015-03-31 17:47:46 -0400

    Add callbacks, scopes and helpers to calenar model

--- app/models/calendar.rb
+++ app/models/calendar.rb
@@ -9,10 +9,32 @@ class Calendar < ActiveRecord::Base
 
   # Callbacks ##################################################################
 
+  before_validation { self.url += "&showTitle=0" }
+  before_save { adjust_primary }
+
   # Scopes #####################################################################
+
+  scope :primary_calendar, -> { find_by(primary: true) }
+  scope :non_primary_calendars, -> { where(primary: false) }
 
   # Associations ###############################################################
 
   # Helpers ####################################################################
 
+  def self.exclude(id)
+    where.not(id: id)
+  end
+
+  private
+
+  def adjust_primary
+    if Calendar.count == 0
+      self.primary = true
+    elsif self.primary
+      old_primary_calendar = Calendar.primary_calendar
+      if old_primary_calendar.present?
+        old_primary_calendar.update(primary: false)
+      end
+    end
+  end
 end
